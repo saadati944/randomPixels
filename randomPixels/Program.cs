e3a9151d7a1c791593707262aecff35677c85f07
saadati944 <saadati944@gmail.com>
2020-04-07 11:15:02 +0430

    Get random pics width.

--- randomPixels/Program.cs
+++ randomPixels/Program.cs
@@ -15,6 +15,12 @@ namespace randomPixels
             {
                 try
                 {
+                    int width = 0;
+                    do
+                    {
+                        Console.Write("Enter width : ");
+                    } while (!int.TryParse(Console.ReadLine(), out width) || width == 0);
+                    Console.WriteLine("width is " + width);
 
                 }
                 catch (Exception ex) { Console.Write("\n\nError : " + ex.Message + "\n\n"); }
