891ad0cc29d2f4f83e083c2a07f07cb73c15b57e
saadati944 <saadati944@gmail.com>
2020-04-07 11:12:39 +0430

    Add system.drawing refrence.

--- randomPixels/Program.cs
+++ randomPixels/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace randomPixels
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
