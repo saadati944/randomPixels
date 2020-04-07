33da05d87c55cfae512d2a0aac50f99a6e94a575
saadati944 <saadati944@gmail.com>
2020-04-07 11:16:29 +0430

    Get green limits.

--- randomPixels/Program.cs
+++ randomPixels/Program.cs
@@ -42,6 +42,22 @@ namespace randomPixels
                         Console.Write("Enter RED end value (from 1 to 255) : ");
                     } while (!int.TryParse(Console.ReadLine(), out r_end) || r_end < 1 || r_end > 255);
                     Console.WriteLine("r_end is " + r_end);
+
+                    int g_start = 0;
+                    do
+                    {
+                        Console.Write("Enter GREEN start value (from 0 to 254) : ");
+                    } while (!int.TryParse(Console.ReadLine(), out g_start) || g_start < 0 || g_start > 254);
+                    Console.WriteLine("g_start is " + g_start);
+
+                    int g_end = 0;
+                    do
+                    {
+                        Console.Write("Enter GREEN end value (from 1 to 255) : ");
+                    } while (!int.TryParse(Console.ReadLine(), out g_end) || g_end < 1 || g_end > 255);
+                    Console.WriteLine("g_end is " + g_end);
+
+
                 }
                 catch (Exception ex) { Console.Write("\n\nError : " + ex.Message + "\n\n"); }
             }
